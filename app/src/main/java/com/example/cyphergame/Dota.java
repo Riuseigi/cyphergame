7387d58a811f75c3ffad52b676ec3a1a37e2b745
Riuseigi <troyliamjarata@gmail.com>
2022-09-26 13:20:24 +0800

    added files

---
+++ app/src/main/java/com/example/cyphergame/Dota.java
@@ -0,0 +1,17 @@
+package com.example.cyphergame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.os.Bundle;
+
+public class Dota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dotalayout);
+
+    }
+}
